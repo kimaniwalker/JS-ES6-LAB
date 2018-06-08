110c0b6c2b6121690353cffd0ddd39504604c509
Kimani Walker <kimaniwalker@gmail.com>
2018-06-08 10:39:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Declare a new function named favMovie that takes in one parameter.
+// Inside the function log a message to the console that tells the user what your favorite movie is.
+function favMovie(movie = 'The Room', name = 'world'){
+    console.log(`My name is ${name} and fav movie is ${movie}`);
+   
+}
+
+// Call the function
+// Now pass in the argument, your favorite movie, when you call the function, update the parameter of the function to have a default value of “The Room”.
+// favMovie('Space Jam');
+
+// Call the function without passing a movie name of your own,
+// Add another parameter to your function so you can pass it a name, give this parameter a default value of “world”
+// Update the string so that you include your name in the message.
+// Call function favMovie by passing it values for both a movie and a name, and also call it without passing it anything.
+
+favMovie('Space Jam','Kimani');
+
+
+
