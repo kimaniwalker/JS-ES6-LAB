f26d7f4ed2049bea4164ce9722832e7262374032
Kimani Walker <kimaniwalker@gmail.com>
2018-06-08 10:58:52 -0500

    Implemented arrow funtions

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Declare a new function named favMovie that takes in one parameter.
 // Inside the function log a message to the console that tells the user what your favorite movie is.
-function favMovie(movie = 'The Room', name = 'world'){
+/* function favMovie(movie = 'The Room', name = 'world'){
     console.log(`My name is ${name} and fav movie is ${movie}`);
    
-}
+} */
 
 // Call the function
 // Now pass in the argument, your favorite movie, when you call the function, update the parameter of the function to have a default value of “The Room”.
@@ -14,7 +14,25 @@ function favMovie(movie = 'The Room', name = 'world'){
 // Update the string so that you include your name in the message.
 // Call function favMovie by passing it values for both a movie and a name, and also call it without passing it anything.
 
-favMovie('Space Jam','Kimani');
+/* favMovie('Space Jam','Kimani'); */
 
+// Arrow Functions
+
+// Change the favMovie function to an ES6 Arrow Function.
+
+
+/* let favMovie = (movie = 'The Room', name = 'world') => {
+    console.log(`My name is ${name} and fav movie is ${movie}`);
+   
+} */
+// Call the function
+// Use the shorthand "concise body" syntax for this arrow function so that the console.log statement is on the same line as the arrow.
+
+let favMovie = (movie = 'The Room', name = 'world') => 
+    console.log(`My name is ${name} and fav movie is ${movie}`);
+   
+
+
+favMovie();
 
 
