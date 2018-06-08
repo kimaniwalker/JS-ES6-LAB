ab12d618e0d1fce3d80807b52ee1ab895540ba52
Kimani Walker <kimaniwalker@gmail.com>
2018-06-08 12:41:22 -0500

    Create new functions

--- script.js
+++ script.js
@@ -28,12 +28,44 @@
 // Call the function
 // Use the shorthand "concise body" syntax for this arrow function so that the console.log statement is on the same line as the arrow.
 
-let favMovie = (movie = 'The Room', name = 'world') => 
+let favMovie = (movie = 'The Room', name = 'world') =>
     console.log(`My name is ${name} and fav movie is ${movie}`);
-   
+
 
 
 favMovie();
+
+// Create a new arrow function called getFirstName
+let fullName = ['Kimani', 'Walker'];
+
+let getFirstName = () => {
+    console.log(fullName[0]);
+}
+
+// Create a second arrow function using the concise body syntax. Both function will serve the same purpose.
+
+let getLastName = () => console.log(fullName[1]);
+
+getFirstName();
+getLastName();
+
+// Create a new function that will take in two parameters and return an object literal.
+// It should have two properties, the exponent result (x^y) and the product of the two numbers.
+// Log each property using a template literal.
+
+let functotal = (x, y) => {
+    let sumtotal = (x + y)
+    let producttotal = (x * y)
+    let expototal = (x ^ y)
+    let divtotal = (x / y)
+
+    console.log(`The total is ${sumtotal}`)
+    console.log(`The product total is ${producttotal}`)
+    console.log(`The exponent total is ${expototal}`)
+    console.log(`The division total is ${divtotal}`)
+}
+
+functotal(4,2);
 
 
 
